bde01cd5f2e06ba7ec86cbd265a2391ddde1eda3
eraldosiniciof <eraldosinicio@gmail.com>
2021-04-04 07:19:42 -0300

    buscar endereco pelo cep

---
+++ readme.md
@@ -0,0 +1,9 @@
+### Buscar endereço a partir de um CEP
+
+Os endereços estão sendo obtidos através do consumo da API da <a href="http://viacep.com.br/" target="_blank">viacep</a>
+
+### Como incluir o CEP
+
+O CEP pode ser digitado de duas formas:
+1. 00000000
+2. 00000-000
---
+++ scripts/buscarCep.js
@@ -0,0 +1,27 @@
+﻿function preencherCampos(resp) {
+	document.querySelector('#endereco').value = resp.logradouro
+	document.querySelector('#complemento').value = resp.complemento
+	document.querySelector('#bairro').value = resp.bairro
+	document.querySelector('#cidade').value = resp.localidade
+	document.querySelector('#estado').value = resp.uf
+}
+
+function consultarCep() {
+	let xhr = new XMLHttpRequest()
+	let cep = document.querySelector('#cep').value.replace('-','')
+	let url = `https://viacep.com.br/ws/${cep}/json/`
+	xhr.open('GET', url, true)
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			if(xhr.status == 200) {
+				preencherCampos(JSON.parse(xhr.responseText))
+			}
+		}
+	}
+	xhr.send()
+}
+
+document.querySelector('#btn-buscar').addEventListener('click', event => {
+	event.preventDefault()
+	consultarCep()
+})
